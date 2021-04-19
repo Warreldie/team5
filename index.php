b0348faff601fbfde997389753f094dd9919497f
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-04-19 21:25:48 +0200

    Started Feature 7 & 8

--- index.php
+++ index.php
@@ -8,15 +8,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IMDtok</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>IMDtok - Feed</title>
 </head>
 
-<body>
-    <li class="nav-item">
-        <a class="nav-link" href="logout.php">Logout</a>
-    </li>
-    <h1>Welcome On IMDTok</h1>
-    <p>Test</p>
+<body class="bg-dark text-white">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#">Following</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#">For You</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="logout.php">Logout</a>
+        </li>
+    </ul>
+
+    <img src="./images/image.jpg" class="img-fluid" alt="IMDTok-video">
+
+    <ul class="navbar navbar-fixed-bottom navbar-inverse">
+        <a class="nav-link text-white" href="#">Home</a>
+        <a class="nav-link text-white" href="#">Discover</a>
+        <a class="nav-link text-white" href="upload.php">New</a>
+        <a class="nav-link text-white" href="#">Inbox</a>
+        <a class="nav-link text-white" href="#">Me</a>
+    </ul>
 </body>
 
 </html>
