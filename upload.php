464e39356c894a1c4b4bc7970af6d955edff5abf
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-04-20 10:36:47 +0200

    Started Feature 7

--- upload.php
+++ upload.php
@@ -1,12 +1,53 @@
+<?
+    if(isset($_POST["submit"])){
+        $file = $_FILES["file"];
+
+        $fileName = $_FILES["file"]["name"];
+        $fileTmpName = $_FILES["file"]["tmp_name"];
+        $fileSize = $_FILES["file"]["size"];
+        $fileError = $_FILES["file"]["error"];
+        $fileType = $_FILES["file"]["type"];
+
+        $fileExt = explode(".", $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+
+        $allowed = array("jpg", "jpeg", "png", "pdf");
+
+        if(in_array($fileActualExt, $allowed)){
+            //Minuut 16 https://www.youtube.com/watch?v=JaRq73y5MJk
+        }else{
+            echo "There was an error uploading your file!";
+        }
+
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
+
 <body>
-    <h1>Upload Here Your Video</h1>
+    <div class="container">
+        <h1>Upload</h1>
+        <form method="POST" action="" enctype="multipart/form-data">
+            <div class="mb-3">
+                <textarea class="form-control" placeholder="Description"></textarea>
+            </div>
+            <img src="./images/image.jpg" class="mb-3 img-fluid" alt="IMDTok-video">
+            <div class="mb-3">
+                <input type="file" name="file" class="mb-3 form-control">
+            </div>
+            <div class="mb-3 d-grid gap-2">
+                <button type="submit" name="submit" class="btn btn-danger">Upload</button>
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
